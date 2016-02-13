68e6e4efed2e735b83afaaed542d4015acadf522
curiosityflow <bmt49@zips.uakron.edu>
2016-02-13 02:23:41 +0000

    Create Advertisement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,16 @@
-class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    @posts.each_with_index do |post, index|
-      if index % 5 == 0
-        post.title = "SPAM"
-      end
-    end
-  end
+ class PostsController < ApplicationController
+   def index
+     @posts = Post.all
+   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+   def show
+ # #19
+     @post = Post.find(params[:id])
+   end
 
-  def new
-    @post = Post.new
-  end
-  
-  def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
-    
-    if @post.save
-      flash[:notice] = "Post was saved."
-      redirect_to @post
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :new
-    end 
-  end
+   def new
+   end
 
-  def edit
-  end
-end
+   def edit
+   end
+ end

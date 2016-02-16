873653d536aa5d07ae1f24ee118a28cea45e2549
curiosityflow <bmt49@zips.uakron.edu>
2016-02-16 17:17:08 +0000

    Create SponsoredPosts

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -1,26 +1,18 @@
-require 'rails_helper'
+RSpec.describe SponsoredPost, type: :model do
+  let(:topic) {Topic.create!(name: RandomData.random_sentence,description: RandomData.random_paragraph)}
+  let(:sponsored_post) { topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 99) }
+  it { should belong_to(:topic) }
 
-RSpec.describe SponsoredPostsController, type: :controller do
+  describe "attributes" do
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "should respond  to title" do
+      expect(sponsored_post).to respond_to(:title)
+    end
+    it "should respond to body" do
+      expect(sponsored_post).to respond_to(:body)
+    end
+    it "should respond to price" do
+      expect(sponsored_post).to respond_to(:price)
     end
   end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end

f58a4e34d929ede3bfa23a6318660b445e7c1ee4
curiosityflow <bmt49@zips.uakron.edu>
2016-02-17 23:58:46 +0000

    Validations assignment

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Topic, type: :model do
      let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
      
      it { is_expected.to have_many(:posts) }
+     it { should validate_length_of(:name).is_at_least(5) }
+     it { should validate_length_of(:description).is_at_least(15) }
+     it { should validate_prsence_of(:name)}
+     it { should validate_presence_of(:description)}
  
  # #1
    describe "attributes" do

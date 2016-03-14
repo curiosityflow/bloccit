7023153cb3a2d9e0acec4b2045218dd8ae14c7df
curiosityflow <bmt49@zips.uakron.edu>
2016-03-14 01:03:01 +0000

    Finish favorites checkpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,16 @@ class User < ActiveRecord::Base
    has_many :posts, dependent: :destroy
    has_many :comments, dependent: :destroy
    has_many :votes, dependent: :destroy
- # #3
+   has_many :favorites, dependent: :destroy
+ 
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
- # #4
+ 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #5
+ 
    validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
    validates :password, length: { minimum: 6 }, allow_blank: true
- # #6
+ 
    validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
@@ -33,5 +34,9 @@ class User < ActiveRecord::Base
            end
            self.name = name_array.join(" ")
        end
+   end
+   
+   def favorite_for(post)
+     favorites.where(post_id: post.id).first
    end
 end
